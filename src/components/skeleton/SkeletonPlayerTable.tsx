deb90c0a865a33b5fa9ad37542c1e821e00dcd94
Emanuel Pecson <emanpecson@gmail.com>
2024-09-07 07:04:29 -0700

    style: :lipstick: refine skeleton-player-table

--- src/components/skeleton/SkeletonPlayerTable.tsx
+++ src/components/skeleton/SkeletonPlayerTable.tsx
@@ -3,37 +3,6 @@ import { playerTableColumns } from '../player/PlayerTable';
 import { Skeleton } from '../ui/skeleton';
 
 export default function SkeletonPlayerTable({ rowCount }: { rowCount: number }) {
-  const defaultSkeleton = (isFirst: boolean, isLast: boolean) => {
-    return (
-      <td className="h-[39px]">
-        <div
-          className={cn(
-            'flex place-items-center',
-            isFirst ? 'justify-start' : isLast ? 'justify-end' : 'justify-center'
-          )}
-        >
-          <Skeleton className="w-4/5 mx-2 h-4" />
-        </div>
-      </td>
-    );
-  };
-
-  const inlineImageSkeleton = (isFirst: boolean, isLast: boolean) => {
-    return (
-      <td className="h-[39px]">
-        <div
-          className={cn(
-            'flex space-x-1.5 place-items-center',
-            isFirst ? 'justify-start' : isLast ? 'justify-end' : 'justify-center'
-          )}
-        >
-          <Skeleton className="rounded-full w-6 h-6" />
-          <Skeleton className="w-2/3 mx-2 h-4" />
-        </div>
-      </td>
-    );
-  };
-
   return (
     <table className="w-full">
       <thead>
@@ -50,20 +19,58 @@ export default function SkeletonPlayerTable({ rowCount }: { rowCount: number }) 
       <tbody className="divide-y">
         {new Array(rowCount).fill(0).map((_, i: number) => (
           <tr key={i} className="sm:text-base text-sm text-neutral-600 dark:text-neutral-200">
-            {playerTableColumns.map((col, j: number) => {
-              const isFirst = j === 0;
-              const isLast = j === playerTableColumns.length - 1;
-              // return (
-              //   <td className="h-[39px]">
-              //     <div className={cn('flex', isFirst ? 'justify-start' : isLast ? 'justify-end' : 'justify-center')}>
-              //       <Skeleton className="w-full mx-2 h-5" />
-              //     </div>
-              //   </td>
-              // );
-              return col === 'player' || col === 'team'
-                ? inlineImageSkeleton(isFirst, isLast)
-                : defaultSkeleton(isFirst, isLast);
-            })}
+            <td className="h-[39px]">
+              <div className={cn('flex space-x-1 place-items-center justify-center')}>
+                <Skeleton className="rounded-full w-6 h-6" />
+                <Skeleton className="w-[12rem] mx-2 h-4" />
+              </div>
+            </td>
+            <td className="h-[39px]">
+              <div className={cn('flex place-items-center justify-center')}>
+                <Skeleton className="w-1/2 mx-2 h-4" />
+              </div>
+            </td>
+            <td className="h-[39px]">
+              <div className={cn('flex place-items-center justify-center')}>
+                <Skeleton className="w-2/3 mx-2 h-4" />
+              </div>
+            </td>
+            <td className="h-[39px]">
+              <div className={cn('flex place-items-center justify-center')}>
+                <Skeleton className="w-2/3 mx-2 h-4" />
+              </div>
+            </td>
+            <td className="h-[39px]">
+              <div className={cn('flex place-items-center justify-center')}>
+                <Skeleton className="w-2/3 mx-2 h-4" />
+              </div>
+            </td>
+            <td className="h-[39px]">
+              <div className={cn('flex place-items-center justify-center')}>
+                <Skeleton className="w-2/3 mx-2 h-4" />
+              </div>
+            </td>
+            <td className="h-[39px]">
+              <div className={cn('flex place-items-center justify-center')}>
+                <Skeleton className="w-2/3 mx-2 h-4" />
+              </div>
+            </td>
+            <td className="h-[39px]">
+              <div className={cn('flex place-items-center justify-center')}>
+                <Skeleton className="w-1/2 mx-2 h-4" />
+              </div>
+            </td>
+            <td className="h-[39px]">
+              <div className={cn('flex space-x-1 place-items-center justify-center')}>
+                <Skeleton className="rounded-full w-6 h-6" />
+                <Skeleton className="w-1/2 mx-2 h-4" />
+              </div>
+            </td>
+            <td className="h-[39px]">
+              <div className={cn('flex place-items-center justify-end')}>
+                <Skeleton className="w-[2.5rem] mx-2 h-4" />
+              </div>
+            </td>
           </tr>
         ))}
       </tbody>
