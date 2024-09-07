94db3a58fe355f68ff6d027f96d5dfcfcc88bf2e
Emanuel Pecson <emanpecson@gmail.com>
2024-09-07 12:16:01 -0700

    style: :lipstick: update formatting of header

--- src/components/player/PlayerDialog.tsx
+++ src/components/player/PlayerDialog.tsx
@@ -12,10 +12,11 @@ interface PlayerDialogProps {
 }
 
 export default function PlayerDialog(props: PlayerDialogProps) {
-  const primaryColor = props.player ? teams[props.player.team as keyof typeof teams].colors[0] : '';
-  const secondaryColor = props.player ? teams[props.player.team as keyof typeof teams].colors[1] : '';
-  const twHeaderHeight = '10rem';
   const teamInfo = teams[props.player?.team as keyof typeof teams];
+  const primaryColor = props.player ? teamInfo.colors[0] : '';
+  const secondaryColor = props.player ? teamInfo.colors[1] : '';
+  const tertiaryColor = props.player && teamInfo.colors.length >= 3 ? teamInfo.colors[2] : '';
+  const headerHeight = '10rem';
 
   return (
     <Dialog
@@ -28,27 +29,35 @@ export default function PlayerDialog(props: PlayerDialogProps) {
         {props.player ? (
           <div>
             <div
-              className="absolute top-0 left-0 rounded-t-lg w-full"
-              style={{ backgroundColor: primaryColor, height: twHeaderHeight }}
+              className="absolute top-0 left-0 rounded-t-lg w-full overflow-clip"
+              style={{ backgroundColor: primaryColor, height: headerHeight }}
             >
               <div className="relative h-full w-full">
+                {/* <div
+                  style={{ backgroundColor: tertiaryColor ?? primaryColor }}
+                  className="rounded-full h-[12rem] w-[12rem] absolute -bottom-24 left-6 overflow-hidden"
+                />
+                <div
+                  style={{ backgroundColor: secondaryColor }}
+                  className="rounded-full h-[16rem] w-[16rem] absolute -bottom-32 -left-20 overflow-hidden"
+                /> */}
                 <Image
                   src={getPlayerPhotoUrl(props.player.nbaId)}
                   alt={props.player?.name}
                   height={24}
                   width={24}
                   unoptimized
-                  className="w-fit h-28 object-cover pl-2 absolute bottom-0"
+                  className="w-fit h-36 object-cover pl-2 absolute bottom-0"
                 />
 
                 <div className="absolute top-2 right-5">
                   <div className="flex place-items-center space-x-2">
-                    <div style={{ color: 'white', textShadow: `2.5x 2.5px 2px ${secondaryColor}` }}>
-                      <h3 className="tracking-wider font-bold text-2xl text-right">{teamInfo.city}</h3>
-                      <h3 className="tracking-wider font-bold text-2xl text-right">{teamInfo.name}</h3>
+                    <div className="text-white">
+                      <h3 className="tracking-wider font-medium text-2xl text-right">{teamInfo.city}</h3>
+                      <h3 className="tracking-wider font-medium text-2xl text-right">{teamInfo.name}</h3>
                     </div>
                     <Image
-                      src={getTeamLogoUrl(teamInfo.nbaId)}
+                      src={getTeamLogoUrl(teamInfo.nbaId, true)}
                       alt={`${teamInfo.abbreviation}-logo`}
                       height={24}
                       width={24}
@@ -58,20 +67,17 @@ export default function PlayerDialog(props: PlayerDialogProps) {
                   </div>
                 </div>
 
-                <div
-                  className="text-3xl font-bold flex space-x-3 tracking-wider absolute bottom-2 right-5"
-                  style={{ color: 'white', textShadow: `2.5x 2.5px 2px ${secondaryColor}` }}
-                >
+                <div className="text-3xl font-bold flex space-x-3 tracking-wider absolute bottom-2 right-5 text-white">
                   <h2>{props.player.jersey}</h2>
                   <h2>{'•'}</h2>
                   <h2>{props.player.name}</h2>
                 </div>
               </div>
             </div>
-            <div style={{ paddingTop: twHeaderHeight }}>test</div>
+            <div style={{ paddingTop: headerHeight }}></div>
           </div>
         ) : (
-          <div>No data</div>
+          <div>Error: No data</div>
         )}
       </DialogContent>
     </Dialog>
