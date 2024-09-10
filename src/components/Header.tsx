1462545b9f71319b962d6cf8d0fd299b0533fbe9
Emanuel Pecson <emanpecson@gmail.com>
2024-09-09 23:43:07 -0700

    style: :lipstick: add width constraint on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ import { ThemeToggle } from './button/ThemeToggle';
 
 export default function Header() {
   return (
-    <div className="">
-      <div className="w-full flex h-24 justify-between border place-items-center px-10 absolute top-0">
+    <div className="w-full absolute top-0 border flex justify-center">
+      <div className="w-full flex h-24 justify-between place-items-center px-10 max-w-[100rem]">
         <h1 className="text-xl font-semibold">{process.env.APP_TITLE ?? 'NBA Fantasy Optimizer'}</h1>
 
         <div>
