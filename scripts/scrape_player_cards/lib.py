f9797682119404d50673f400e807df9b53dc7c17
Emanuel Pecson <emanpecson@gmail.com>
2024-09-29 18:42:55 -0700

    chore: :rewind: revert back to print-functions in lib file

--- scripts/scrape_player_cards/lib.py
+++ scripts/scrape_player_cards/lib.py
@@ -9,12 +9,27 @@ import csv
 import os
 import time
 
-from scripts.print import alert
-
 ###############################################################################
 # CONSTANTS
 
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RED = '\033[91m'
+RESET = '\033[0m'
+
 LOG_PATH = f'{os.getcwd()}/scripts/scrape_player_cards/log'
+
+###############################################################################
+# PRINT MESSAGES
+
+def alert(msg):
+	print(YELLOW + msg + RESET)
+
+def success(msg):
+	print(GREEN + msg + RESET)
+
+def fail(msg):
+	print(RED + msg + RESET)
 
 ###############################################################################
 # PARSE FUNC
