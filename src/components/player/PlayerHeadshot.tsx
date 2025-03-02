68aa9f91619c7fe1c417367f219511f6d18bea0e
Emanuel Pecson <emanpecson@gmail.com>
2025-03-01 17:11:57 -0800

    feat: :sparkles: add size options for player-headshot

--- src/components/player/PlayerHeadshot.tsx
+++ src/components/player/PlayerHeadshot.tsx
@@ -1,22 +1,42 @@
 import { cn } from '@/lib/utils';
 import { PlayerIncludeRegularStats } from '@/types/response/player/PlayerIncludeRegularStats';
-import { getPlayerPhotoUrl } from '@/utils/getPhotoUrl';
+import { getPlayerPhotoUrl, getTeamLogoUrl } from '@/utils/getPhotoUrl';
 import Image from 'next/image';
 
 interface PlayerHeadshotProps {
   player: PlayerIncludeRegularStats | null;
-  teamLogoUrl: string;
+  size?: 'sm' | 'md' | 'lg';
 }
 
 export default function PlayerHeadshot(props: PlayerHeadshotProps) {
+  const teamLogoUrl = props.player ? getTeamLogoUrl(props.player.teamAbbreviation) : '';
+
+  const bgSizeStyle =
+    props.size === 'sm'
+      ? 'h-28 w-32'
+      : props.size === 'md'
+        ? 'h-40 w-48'
+        : props.size === 'lg'
+          ? 'h-60 w-72'
+          : 'h-28 w-32';
+
+  const imageSizeStyle =
+    props.size === 'sm'
+      ? 'h-36 w-32'
+      : props.size === 'md'
+        ? 'h-52 w-48'
+        : props.size === 'lg'
+          ? 'h-80 w-72'
+          : 'h-36 w-32';
+
   return (
-    <div className="rounded-2xl bg-neutral-200 dark:bg-neutral-800 h-28 w-32">
+    <div className={cn(bgSizeStyle, 'rounded-2xl bg-neutral-200 dark:bg-neutral-800 relative')}>
       <Image
         src={getPlayerPhotoUrl(props.player?.nbaId ?? '')}
         alt={props.player?.name ?? ''}
         height={24}
         width={24}
-        className={cn(!props.teamLogoUrl && 'opacity-30', 'h-36 w-32 object-cover absolute bottom-0 rounded-b-2xl')}
+        className={cn(!teamLogoUrl && 'opacity-30', imageSizeStyle, 'object-cover absolute bottom-0 rounded-b-2xl')}
         unoptimized
       />
     </div>
