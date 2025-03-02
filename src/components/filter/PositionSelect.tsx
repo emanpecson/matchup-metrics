5acf3f382f296ddddcc78405c5e3bf40c5d4789a
Emanuel Pecson <emanpecson@gmail.com>
2025-03-01 16:22:23 -0800

    refactor: :recycle: use props in position-select

--- src/components/filter/PositionSelect.tsx
+++ src/components/filter/PositionSelect.tsx
@@ -1,35 +1,33 @@
 import { useState } from 'react';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select';
+import { Select, SelectContent, SelectTrigger } from '../ui/select';
 import { PuzzlePieceIcon } from '@heroicons/react/24/outline';
 import { cn } from '@/lib/utils';
-import { Button } from '../ui/button';
-import { CheckIcon, XIcon } from 'lucide-react';
+import { CheckIcon } from 'lucide-react';
 import positions from '@/data/positions';
 import CloseButton from '../button/CloseButton';
 
-export default function PositionSelect({
-  value,
-  onValueChange,
-}: {
+interface PositionSelectProps {
   value: string;
   onValueChange: (pos: string) => void;
-}) {
+}
+
+export default function PositionSelect(props: PositionSelectProps) {
   const [isOpen, setIsOpen] = useState(false);
   const [isHovering, setIsHovering] = useState(false);
 
   const handleSelect = (currValue: string) => {
-    onValueChange(currValue);
+    props.onValueChange(currValue);
   };
 
   const handleReset = () => {
-    onValueChange('');
+    props.onValueChange('');
   };
 
   return (
     <Select onOpenChange={setIsOpen} open={isOpen}>
       <div className="relative" onMouseOver={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>
         <div className="absolute left-2 top-2 pointer-events-none">
-          {value ? (
+          {props.value ? (
             <CloseButton onClick={handleReset} className="pointer-events-auto" />
           ) : (
             <PuzzlePieceIcon
@@ -43,12 +41,12 @@ export default function PositionSelect({
         <SelectTrigger className="min-w-48 space-x-4 font-medium hover:bg-neutral-200 dark:hover:bg-neutral-900 text-neutral-500 dark:hover:text-white hover:text-neutral-800 duration-150 transition-colors">
           <div
             className={cn(
-              value ? 'dark:text-white text-neutral-800' : '',
+              props.value ? 'dark:text-white text-neutral-800' : '',
               'flex space-x-1.5 place-items-center text-nowrap'
             )}
           >
             <label className="cursor-pointer pl-7">
-              {value ? positions[value as keyof typeof positions] : 'Filter Position'}
+              {props.value ? positions[props.value as keyof typeof positions] : 'Filter Position'}
             </label>
           </div>
         </SelectTrigger>
@@ -59,13 +57,13 @@ export default function PositionSelect({
           // <SelectItem value={entry[0]} isSelected={entry[0] === value} key={i} className="cursor-pointer">
           //   {entry[1]}
           // </SelectItem>
-          const isSelected = entry[0] === value;
+          const isSelected = entry[0] === props.value;
 
           return (
             <button
               key={i}
               onClick={() => {
-                onValueChange(isSelected ? '' : entry[0]);
+                props.onValueChange(isSelected ? '' : entry[0]);
                 setIsOpen(false);
               }}
               className="flex h-10 w-full items-center rounded-md bg-background px-3 text-sm dark:hover:bg-neutral-800 hover:bg-neutral-100"
