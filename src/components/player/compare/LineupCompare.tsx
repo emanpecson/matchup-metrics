a911ce9214315c61c3b7f92bf8d68edcc2fa98d3
Emanuel Pecson <emanpecson@gmail.com>
2025-03-04 19:28:49 -0800

    style: :lipstick: use different ui for single or no player lineups

--- src/components/player/compare/LineupCompare.tsx
+++ src/components/player/compare/LineupCompare.tsx
@@ -3,6 +3,12 @@ import LineupStatsCompare from './LineupStatsCompare';
 import LineupSlot, { LineupSlotState } from '@/components/lineup/slot/LineupSlot';
 import { Dispatch, SetStateAction } from 'react';
 import { cn } from '@/lib/utils';
+import { reformatHeight, reformatPosition } from '@/utils/reformatString';
+import positions from '@/data/positions';
+import Image from 'next/image';
+import { getTeamLogoUrl } from '@/utils/getPhotoUrl';
+import teams, { TeamInfo } from '@/data/teams';
+import PlayerHeadshot from '../PlayerHeadshot';
 
 interface LineupCompareProps {
   lineup1: LineupBuilder;
@@ -14,8 +20,8 @@ interface LineupCompareProps {
 export default function LineupCompare(props: LineupCompareProps) {
   /**
    * Removes a player at a given lineup slot id.
-   * @param {LineupBuilder} - Lineup to perform removal on
-   * @param {Dispatch<SetStateAction<LineupBuilder>>} - Updates lineup reactively
+   * @param {LineupBuilder} lineup - Lineup to perform removal on
+   * @param {Dispatch<SetStateAction<LineupBuilder>>} setLineup - Updates lineup reactively
    * @param {number} slotId - Removal position
    */
   const handleLineupSlotRemove = (
@@ -31,35 +37,89 @@ export default function LineupCompare(props: LineupCompareProps) {
   /**
    * Displays players within lineup dynamically
    * @param param0 - Lineup to display
+   * @param param1 - Updates lineup
    * @returns {JSX.Element}
    */
-  const DisplayLineup = ({
-    lineup,
-    setLineup,
-  }: {
+  const DisplayLineup = (param: {
     lineup: LineupBuilder;
     setLineup: Dispatch<SetStateAction<LineupBuilder>>;
-  }) => {
+  }): JSX.Element => {
+    const slots = param.lineup.getLineup().filter((slot) => slot.player);
+
+    // if no players
+    if (param.lineup.getSize() === 0) {
+      return (
+        <div className="flex place-items-center px-16">
+          <div className="space-y-0.5">
+            <PlayerHeadshot player={null} size="lg" />
+            <h3 className="font-extrabold text-xl text-center text-neutral-500">Add player</h3>
+          </div>
+        </div>
+      );
+    }
+
+    // if single player
+    if (param.lineup.getSize() === 1) {
+      return (
+        <div className="flex place-items-center px-16">
+          {slots.map((slot) => {
+            const team = teams[slot.player!.teamAbbreviation as keyof typeof teams];
+            return (
+              <div className="space-y-0.5">
+                <LineupSlot
+                  player={slot.player}
+                  key={slot.id}
+                  position={slot.lineupPosition}
+                  state={LineupSlotState.REMOVE}
+                  onClick={() => handleLineupSlotRemove(param.lineup, param.setLineup, slot.id)}
+                  size="lg"
+                  hideCaption
+                  hideTeam
+                />
+                <h3 className="font-extrabold text-xl text-center text-neutral-800 dark:text-neutral-200">
+                  {slot.player!.name}
+                </h3>
+                <div className="flex space-x-1 text-center justify-center font-medium text-nowrap">
+                  <span>{reformatHeight(slot.player!.height)},</span>
+                  <span>{slot.player!.weight},</span>
+                  <span>{reformatPosition(slot.player!.position as keyof typeof positions)}</span>
+                </div>
+                <div className="flex space-x-1 justify-center font-medium place-items-center">
+                  <Image
+                    src={getTeamLogoUrl(team.abbreviation)}
+                    alt={team.abbreviation}
+                    height={28}
+                    width={28}
+                    unoptimized
+                  />
+                  <span>{team.city}</span>
+                  <span>{team.name}</span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+
+    // otherwise, display lineup
     return (
       <div
         className={cn(
           'flex flex-wrap w-[60rem] justify-center place-items-center',
-          lineup.getSize() <= 4 ? 'gap-9' : 'gap-5'
+          param.lineup.getSize() <= 4 ? 'gap-9' : 'gap-5'
         )}
       >
-        {lineup
-          .getLineup()
-          .filter((slot) => slot.player)
-          .map((slot) => (
-            <LineupSlot
-              player={slot.player}
-              key={slot.id}
-              position={slot.lineupPosition}
-              state={slot.player ? LineupSlotState.REMOVE : LineupSlotState.STATIC}
-              onClick={slot.player ? () => handleLineupSlotRemove(lineup, setLineup, slot.id) : undefined}
-              size={lineup.getSize() === 1 ? 'lg' : lineup.getSize() <= 4 ? 'md' : 'sm'}
-            />
-          ))}
+        {slots.map((slot) => (
+          <LineupSlot
+            player={slot.player}
+            key={slot.id}
+            position={slot.lineupPosition}
+            state={LineupSlotState.REMOVE}
+            onClick={() => handleLineupSlotRemove(param.lineup, param.setLineup, slot.id)}
+            size={param.lineup.getSize() <= 4 ? 'md' : 'sm'}
+          />
+        ))}
       </div>
     );
   };
