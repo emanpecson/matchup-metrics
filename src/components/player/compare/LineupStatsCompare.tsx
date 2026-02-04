beb8cdf009a79207ec64f4e827fe5fbcf63b77ee
bmo <emanpecson@gmail.com>
2026-02-03 21:02:39 -0800

    refactor: :recycle: move more inner components outside to prevent rendering issues

--- src/components/player/compare/LineupStatsCompare.tsx
+++ src/components/player/compare/LineupStatsCompare.tsx
@@ -7,69 +7,59 @@ interface LineupStatsCompareProps {
   lineup2: LineupBuilder;
 }
 
+function DisplayLineupAverageStats(props: {
+  lineup: LineupBuilder;
+  vsLineup: LineupBuilder;
+  justifyRight?: boolean;
+}): JSX.Element {
+  const stats = props.lineup.getLineupStats();
+  const vsStats = props.vsLineup.getLineupStats();
+
+  return (
+    <div className="divide-y w-full">
+      <FormatStat stat={stats.fantasyAvg} vsStat={vsStats.fantasyAvg} justifyRight={props.justifyRight} />
+      <FormatStat stat={stats.pointsAvg} vsStat={vsStats.pointsAvg} justifyRight={props.justifyRight} />
+      <FormatStat stat={stats.assistsAvg} vsStat={vsStats.assistsAvg} justifyRight={props.justifyRight} />
+      <FormatStat stat={stats.reboundsAvg} vsStat={vsStats.reboundsAvg} justifyRight={props.justifyRight} />
+      <FormatStat stat={stats.stealsAvg} vsStat={vsStats.stealsAvg} justifyRight={props.justifyRight} />
+      <FormatStat stat={stats.blocksAvg} vsStat={vsStats.blocksAvg} justifyRight={props.justifyRight} />
+      <FormatStat stat={stats.turnoversAvg} vsStat={vsStats.turnoversAvg} justifyRight={props.justifyRight} reverse />
+      <FormatStat stat={stats.minutesAvg} vsStat={vsStats.minutesAvg} justifyRight={props.justifyRight} />
+      <FormatStat stat={stats.plusMinusAvg} vsStat={vsStats.plusMinusAvg} justifyRight={props.justifyRight} />
+      <FormatStat stat={stats.fgPct} vsStat={vsStats.fgPct} justifyRight={props.justifyRight} showPercent />
+      <FormatStat stat={stats.fg3Pct} vsStat={vsStats.fg3Pct} justifyRight={props.justifyRight} showPercent />
+      <FormatStat stat={stats.ftPct} vsStat={vsStats.ftPct} justifyRight={props.justifyRight} showPercent />
+    </div>
+  );
+}
+
+function DisplayLineupSumStats(props: {
+  lineup: LineupBuilder;
+  vsLineup: LineupBuilder;
+  justifyRight?: boolean;
+}): JSX.Element {
+  const stats = props.lineup.getLineupStats();
+  const vsStats = props.vsLineup.getLineupStats();
+
+  return (
+    <div className="divide-y w-full">
+      <FormatStat stat={stats.fantasySum} vsStat={vsStats.fantasySum} justifyRight={props.justifyRight} />
+      <FormatStat stat={stats.pointsSum} vsStat={vsStats.pointsSum} justifyRight={props.justifyRight} />
+      <FormatStat stat={stats.assistsSum} vsStat={vsStats.assistsSum} justifyRight={props.justifyRight} />
+      <FormatStat stat={stats.reboundsSum} vsStat={vsStats.reboundsSum} justifyRight={props.justifyRight} />
+      <FormatStat stat={stats.stealsSum} vsStat={vsStats.stealsSum} justifyRight={props.justifyRight} />
+      <FormatStat stat={stats.blocksSum} vsStat={vsStats.blocksSum} justifyRight={props.justifyRight} />
+      <FormatStat stat={stats.turnoversSum} vsStat={vsStats.turnoversSum} justifyRight={props.justifyRight} reverse />
+      <FormatStat stat={stats.minutesSum} vsStat={vsStats.minutesSum} justifyRight={props.justifyRight} />
+      <FormatStat stat={stats.plusMinusSum} vsStat={vsStats.plusMinusSum} justifyRight={props.justifyRight} />
+      <FormatStat stat={undefined} vsStat={undefined} justifyRight={props.justifyRight} />
+      <FormatStat stat={undefined} vsStat={undefined} justifyRight={props.justifyRight} />
+      <FormatStat stat={undefined} vsStat={undefined} justifyRight={props.justifyRight} />
+    </div>
+  );
+}
+
 export default function LineupStatsCompare(props: LineupStatsCompareProps) {
-  /**
-   * Displays lineups' stat averages
-   * @param {Object} props - Source for stats
-   * @returns {JSX.Element}
-   */
-  const DisplayLineupAverageStats = (props: {
-    lineup: LineupBuilder;
-    vsLineup: LineupBuilder;
-    justifyRight?: boolean;
-  }): JSX.Element => {
-    const stats = props.lineup.getLineupStats();
-    const vsStats = props.vsLineup.getLineupStats();
-
-    return (
-      <div className="divide-y w-full">
-        <FormatStat stat={stats.fantasyAvg} vsStat={vsStats.fantasyAvg} justifyRight={props.justifyRight} />
-        <FormatStat stat={stats.pointsAvg} vsStat={vsStats.pointsAvg} justifyRight={props.justifyRight} />
-        <FormatStat stat={stats.assistsAvg} vsStat={vsStats.assistsAvg} justifyRight={props.justifyRight} />
-        <FormatStat stat={stats.reboundsAvg} vsStat={vsStats.reboundsAvg} justifyRight={props.justifyRight} />
-        <FormatStat stat={stats.stealsAvg} vsStat={vsStats.stealsAvg} justifyRight={props.justifyRight} />
-        <FormatStat stat={stats.blocksAvg} vsStat={vsStats.blocksAvg} justifyRight={props.justifyRight} />
-        <FormatStat stat={stats.turnoversAvg} vsStat={vsStats.turnoversAvg} justifyRight={props.justifyRight} reverse />
-        <FormatStat stat={stats.minutesAvg} vsStat={vsStats.minutesAvg} justifyRight={props.justifyRight} />
-        <FormatStat stat={stats.plusMinusAvg} vsStat={vsStats.plusMinusAvg} justifyRight={props.justifyRight} />
-        <FormatStat stat={stats.fgPct} vsStat={vsStats.fgPct} justifyRight={props.justifyRight} showPercent />
-        <FormatStat stat={stats.fg3Pct} vsStat={vsStats.fg3Pct} justifyRight={props.justifyRight} showPercent />
-        <FormatStat stat={stats.ftPct} vsStat={vsStats.ftPct} justifyRight={props.justifyRight} showPercent />
-      </div>
-    );
-  };
-
-  /**
-   * Displays lineups' stat sums
-   * @param {Object} props - Source for stats
-   * @returns {JSX.Element}
-   */
-  const DisplayLineupSumStats = (props: {
-    lineup: LineupBuilder;
-    vsLineup: LineupBuilder;
-    justifyRight?: boolean;
-  }): JSX.Element => {
-    const stats = props.lineup.getLineupStats();
-    const vsStats = props.vsLineup.getLineupStats();
-
-    return (
-      <div className="divide-y w-full">
-        <FormatStat stat={stats.fantasySum} vsStat={vsStats.fantasySum} justifyRight={props.justifyRight} />
-        <FormatStat stat={stats.pointsSum} vsStat={vsStats.pointsSum} justifyRight={props.justifyRight} />
-        <FormatStat stat={stats.assistsSum} vsStat={vsStats.assistsSum} justifyRight={props.justifyRight} />
-        <FormatStat stat={stats.reboundsSum} vsStat={vsStats.reboundsSum} justifyRight={props.justifyRight} />
-        <FormatStat stat={stats.stealsSum} vsStat={vsStats.stealsSum} justifyRight={props.justifyRight} />
-        <FormatStat stat={stats.blocksSum} vsStat={vsStats.blocksSum} justifyRight={props.justifyRight} />
-        <FormatStat stat={stats.turnoversSum} vsStat={vsStats.turnoversSum} justifyRight={props.justifyRight} reverse />
-        <FormatStat stat={stats.minutesSum} vsStat={vsStats.minutesSum} justifyRight={props.justifyRight} />
-        <FormatStat stat={stats.plusMinusSum} vsStat={vsStats.plusMinusSum} justifyRight={props.justifyRight} />
-        <FormatStat stat={undefined} vsStat={undefined} justifyRight={props.justifyRight} />
-        <FormatStat stat={undefined} vsStat={undefined} justifyRight={props.justifyRight} />
-        <FormatStat stat={undefined} vsStat={undefined} justifyRight={props.justifyRight} />
-      </div>
-    );
-  };
-
   return (
     <table className="w-full">
       <thead>
--- src/components/player/compare/PlayerStatsCompareTable.tsx
+++ src/components/player/compare/PlayerStatsCompareTable.tsx
@@ -9,69 +9,59 @@ interface PlayerStatsCompareProps {
   player2: PlayerIncludeRegularStats | null;
 }
 
+function FormatStat(props: {
+  stat?: number;
+  vsStat?: number;
+  justifyRight?: boolean;
+  reverse?: boolean;
+}): JSX.Element {
+  return (
+    <div
+      className={cn(props.justifyRight ? 'justify-end' : 'justify-start', 'flex space-x-1 place-items-center w-full')}
+    >
+      {!props.justifyRight && (
+        <StatCompareIcon thisStat={props.stat ?? 0} thatStat={props.vsStat ?? 0} reverse={props.reverse} />
+      )}
+      <span className="font-semibold">{props.stat}</span>
+      {props.justifyRight && (
+        <StatCompareIcon thisStat={props.stat ?? 0} thatStat={props.vsStat ?? 0} reverse={props.reverse} />
+      )}
+    </div>
+  );
+}
+
+function PlayerStats(props: {
+  player: PlayerIncludeRegularStats | null;
+  vsPlayer: PlayerIncludeRegularStats | null;
+  justifyRight?: boolean;
+}): JSX.Element {
+  const p1Stats = props.player?.regularStats;
+  const p2Stats = props.vsPlayer?.regularStats;
+
+  return (
+    <div className="divide-y w-full">
+      <FormatStat
+        stat={p1Stats ? (yahooFantasyPpg(p1Stats) as unknown as number) : 0}
+        vsStat={p2Stats ? (yahooFantasyPpg(p2Stats) as unknown as number) : 0}
+        justifyRight={props.justifyRight}
+      />
+      <FormatStat stat={p1Stats?.ppg ?? 0} vsStat={p2Stats?.ppg ?? 0} justifyRight={props.justifyRight} />
+      <FormatStat stat={p1Stats?.apg ?? 0} vsStat={p2Stats?.apg ?? 0} justifyRight={props.justifyRight} />
+      <FormatStat stat={p1Stats?.rpg ?? 0} vsStat={p2Stats?.rpg ?? 0} justifyRight={props.justifyRight} />
+      <FormatStat stat={p1Stats?.spg ?? 0} vsStat={p2Stats?.spg ?? 0} justifyRight={props.justifyRight} />
+      <FormatStat stat={p1Stats?.bpg ?? 0} vsStat={p2Stats?.bpg ?? 0} justifyRight={props.justifyRight} />
+      <FormatStat stat={p1Stats?.plusMinus ?? 0} vsStat={p2Stats?.plusMinus ?? 0} justifyRight={props.justifyRight} />
+      <FormatStat stat={p1Stats?.mpg ?? 0} vsStat={p2Stats?.mpg ?? 0} justifyRight={props.justifyRight} />
+      <FormatStat stat={p1Stats?.fgPct ?? 0} vsStat={p2Stats?.fgPct ?? 0} justifyRight={props.justifyRight} />
+      <FormatStat stat={p1Stats?.fg3Pct ?? 0} vsStat={p2Stats?.fg3Pct ?? 0} justifyRight={props.justifyRight} />
+      <FormatStat stat={p1Stats?.ftPct ?? 0} vsStat={p2Stats?.ftPct ?? 0} justifyRight={props.justifyRight} />
+      <FormatStat stat={p1Stats?.tpg ?? 0} vsStat={p2Stats?.tpg ?? 0} justifyRight={props.justifyRight} reverse />
+      <FormatStat stat={p1Stats?.gp ?? 0} vsStat={p2Stats?.gp ?? 0} justifyRight={props.justifyRight} />
+    </div>
+  );
+}
+
 export default function PlayerStatsCompareTable(props: PlayerStatsCompareProps) {
-  /**
-   * Displays comparison of a stat
-   * @param {Object} props - Stats to compare
-   * @returns {JSX.Element}
-   */
-  const FormatStat = (props: {
-    stat?: number;
-    vsStat?: number;
-    justifyRight?: boolean;
-    reverse?: boolean;
-  }): JSX.Element => {
-    return (
-      <div
-        className={cn(props.justifyRight ? 'justify-end' : 'justify-start', 'flex space-x-1 place-items-center w-full')}
-      >
-        {!props.justifyRight && (
-          <StatCompareIcon thisStat={props.stat ?? 0} thatStat={props.vsStat ?? 0} reverse={props.reverse} />
-        )}
-        <span className="font-semibold">{props.stat}</span>
-        {props.justifyRight && (
-          <StatCompareIcon thisStat={props.stat ?? 0} thatStat={props.vsStat ?? 0} reverse={props.reverse} />
-        )}
-      </div>
-    );
-  };
-
-  /**
-   * Displays player stats
-   * @param {Object} props - Source for stats
-   * @returns {JSX.Element}
-   */
-  const PlayerStats = (props: {
-    player: PlayerIncludeRegularStats | null;
-    vsPlayer: PlayerIncludeRegularStats | null;
-    justifyRight?: boolean;
-  }): JSX.Element => {
-    const p1Stats = props.player?.regularStats;
-    const p2Stats = props.vsPlayer?.regularStats;
-
-    return (
-      <div className="divide-y w-full">
-        <FormatStat
-          stat={p1Stats ? (yahooFantasyPpg(p1Stats) as unknown as number) : 0}
-          vsStat={p2Stats ? (yahooFantasyPpg(p2Stats) as unknown as number) : 0}
-          justifyRight={props.justifyRight}
-        />
-        <FormatStat stat={p1Stats?.ppg ?? 0} vsStat={p2Stats?.ppg ?? 0} justifyRight={props.justifyRight} />
-        <FormatStat stat={p1Stats?.apg ?? 0} vsStat={p2Stats?.apg ?? 0} justifyRight={props.justifyRight} />
-        <FormatStat stat={p1Stats?.rpg ?? 0} vsStat={p2Stats?.rpg ?? 0} justifyRight={props.justifyRight} />
-        <FormatStat stat={p1Stats?.spg ?? 0} vsStat={p2Stats?.spg ?? 0} justifyRight={props.justifyRight} />
-        <FormatStat stat={p1Stats?.bpg ?? 0} vsStat={p2Stats?.bpg ?? 0} justifyRight={props.justifyRight} />
-        <FormatStat stat={p1Stats?.plusMinus ?? 0} vsStat={p2Stats?.plusMinus ?? 0} justifyRight={props.justifyRight} />
-        <FormatStat stat={p1Stats?.mpg ?? 0} vsStat={p2Stats?.mpg ?? 0} justifyRight={props.justifyRight} />
-        <FormatStat stat={p1Stats?.fgPct ?? 0} vsStat={p2Stats?.fgPct ?? 0} justifyRight={props.justifyRight} />
-        <FormatStat stat={p1Stats?.fg3Pct ?? 0} vsStat={p2Stats?.fg3Pct ?? 0} justifyRight={props.justifyRight} />
-        <FormatStat stat={p1Stats?.ftPct ?? 0} vsStat={p2Stats?.ftPct ?? 0} justifyRight={props.justifyRight} />
-        <FormatStat stat={p1Stats?.tpg ?? 0} vsStat={p2Stats?.tpg ?? 0} justifyRight={props.justifyRight} reverse />
-        <FormatStat stat={p1Stats?.gp ?? 0} vsStat={p2Stats?.gp ?? 0} justifyRight={props.justifyRight} />
-      </div>
-    );
-  };
-
   return (
     <table className="w-full">
       <tbody>
